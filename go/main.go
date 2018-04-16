8bb3c22dd960baa8d8830b7f4218f59f52e3ff60
mewa <marcin.k.chmiel@gmail.com>
2018-04-16 16:05:34 +0200

    Add client

---
+++ go/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Print("Enter number of expressions: ")
+	reader := bufio.NewReader(os.Stdin)
+
+	line, _ := reader.ReadString('\n')
+	nExprsStr := strings.TrimSpace(line)
+	numExprs, err := strconv.Atoi(nExprsStr)
+
+	if err != nil {
+		fmt.Println("Argument should be a number", err)
+	}
+
+	args := make([]string, numExprs + 1)
+	args[0] = nExprsStr
+
+	for i := 1; i <= numExprs; i = i + 1 {
+		line, _ = reader.ReadString('\n')
+		arg := strings.TrimSpace(line)
+		args[i] = arg
+	}
+
+	cmd := exec.Command("python/api.py", args...)
+	cmd.Stderr = os.Stderr
+
+	out, err := cmd.Output()
+
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+
+	fmt.Print(string(out))
+}
